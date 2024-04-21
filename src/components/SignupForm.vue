723c88e0bd64b62d0b8401c35bbc31d5e6115402
Ashok <ashok@N14545.local>
2024-04-21 10:22:14 +0530

    signup form added

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,24 +1,75 @@
 
 <template>
-  <div>
-    <p>Count is: {{ count }}</p>
-    <v-btn @click="increment" color="primary"> Button </v-btn>
-  </div>
+  <v-container class="signup-form" fluid>
+    <v-row justify="center" align="center" class="signup-form-row">
+      <v-col cols="6">
+        <img src="../../public/chat.jpeg" alt="Chat Image" class="image" />
+      </v-col>
+      <v-col cols="6">
+        <v-card class="mx-auto" max-width="500">
+          <v-card-title class="headline">Sign Up</v-card-title>
+          <v-card-text>
+            <v-form @submit.prevent="submitForm">
+              <v-text-field
+                v-model="userForm.username"
+                label="Username"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="userForm.email"
+                label="Email"
+                type="email"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="userForm.password"
+                label="Password"
+                type="password"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="userForm.status"
+                label="Status"
+                required
+              ></v-text-field>
+              <v-btn type="submit" color="primary">Sign Up</v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
 
-// reactive state
-const count = ref(0);
+const userForm = ref({
+  username: "",
+  email: "",
+  password: "",
+  status: "",
+});
 
-// functions that mutate state and trigger updates
-function increment() {
-  count.value++;
-}
+const submitForm = () => {
+  console.log(userForm.value);
+};
 
 // lifecycle hooks
 onMounted(() => {
-  console.log(`The initial count is ${count.value}.`);
+  console.log("mounted");
 });
 </script>
+
+<style scoped>
+.image {
+  max-width: 100%;
+  height: auto;
+}
+.signup-form {
+  padding: 0px;
+}
+.signup-form-row {
+  height: 100vh;
+}
+</style>
